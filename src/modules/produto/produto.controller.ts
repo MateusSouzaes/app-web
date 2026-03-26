c77ec9be85a72551806388623343b5777739f2a9
Mateus Souza e Silva <2023202060007@aluno.ifro.edu.br>
2026-03-25 20:04:26 -0400

    Hello

--- src/modules/produto/produto.controller.ts
+++ src/modules/produto/produto.controller.ts
@@ -1,24 +1,12 @@
 import { Controller, Get, Render } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+@Controller('produto')
+export class ProdutoController {
   @Get()
-  @Render('inicial')
-  getHello(): object {
-    
-    let pessoas = [
-      {nome: 'Maria', email: 'maria@example.com'},
-      {nome: 'João', email: 'joao@example.com'},
-      {nome: 'Ana', email: 'ana@example.com'},
-      {nome: 'Pedro', email: 'pedro@example.com'}
-    ]
-    return { 
-      titulo: 'AppWeb com NestJs', 
-      horaAgora: new Date().toLocaleTimeString(),
-      listaPessoas: pessoas
+  @Render('produto/inicial')
+  getProduto(): object {
+    return {
+      titulo: "Produtos - consulta"
+    }
     };
-  }
-}
+ }
